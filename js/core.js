eb3b3246fc16cae77d8a2d213b27ecc5df527047
이한솔 <esori0v0@gmail.com>
2026-06-30 11:58:13 +0900

    png변경, 로비변경

--- js/core.js
+++ js/core.js
@@ -95,7 +95,7 @@ const ASSET_PATHS = {
   mageSprite: "assets/animations/mage/red_wizard_spritesheet.png",
   saintessSprite: "assets/animations/saintess/saintess_spritesheet_aligned.png",
   heroSprite: "assets/animations/hero/zeus_hero_spritesheet_latest_transparent_aligned.png",
-  lobbyHeroIdle: "assets/animations/hero/zeus_lobby_idle.png",
+  lobbyHeroIdle: "assets/animations/hero/zeus_lobby_idle_hd.png",
   stage1EnemySprite: "assets/animations/enemy/stage1_goblin_spritesheet.png",
   stage1Background: "assets/maps/stage1/stage1_forest_bg_v2.png",
   stageBackgroundTemplate: "assets/maps/stage{stage}/stage{stage}_background.png",
