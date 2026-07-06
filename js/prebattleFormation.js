edb0470c15662a61f9f07fcbc476fbdbc006dee8
병훈 박 <ujdl130@gmail.com>
2026-07-07 03:19:36 +0900

    수정7

--- js/prebattleFormation.js
+++ js/prebattleFormation.js
@@ -17,9 +17,8 @@ function renderPrebattleFormation() {
 
   root.innerHTML = `
     <div class="prebattle-formation-board">
-      <span class="prebattle-formation-arrow prebattle-formation-arrow-left" aria-hidden="true"></span>
       <img class="prebattle-formation-hero-card" src="assets/maps/formation/zeus.png" alt="제우스 카드" draggable="false">
-      <span class="prebattle-formation-arrow prebattle-formation-arrow-right" aria-hidden="true"></span>
+      <img class="prebattle-formation-arrow prebattle-formation-arrow-right" src="assets/ui/arrow_right_redesign.png" alt="" aria-hidden="true" draggable="false">
     </div>
   `;
 }
